307b01f165c27219c694f4b7844619d7aabd1fb9
Patrick Hung <78626322+TheFishEy@users.noreply.github.com>
2024-05-19 23:24:11 -0700

    Uploaded the template instead of my edited project 3 file the first time, sorry about that and thanks!

--- stack _queue_template-3.cpp
+++ stack _queue_template-3.cpp
@@ -2,346 +2,136 @@
 #include<string>
 #include<vector>
 #include<utility>
-#include <algorithm>
+#include<algorithm>
 
-// Logan Arroyo
-//CWID: 884643172
+// Patrick Hung
+//CWID: 860865329
 
-// Add class Stack here
-class Stack
-{
+class Stack {
     private:
-        void push(std::vector<std::pair<char, int>>& a, std::pair<char, int> b)
-        {
+        void push(std::vector<std::pair<char, int>>& a, std::pair<char, int> b) {
             a.push_back(b);
         }
-        std::pair<char, int> top(std::vector<std::pair<char, int>>& a)
-        {
-            if (a.size() == 0)
-            {
-                return std::make_pair(NULL, NULL);
+        std::pair<char, int> top(std::vector<std::pair<char, int>>& a) {
+            if (a.empty()) {
+                return std::make_pair('\0', -1);
             }
-            return a[a.size()-1];
+            return a.back();
         }
-        void pop(std::vector<std::pair<char, int>>& a)
-        {
-            a.pop_back();
+        void pop(std::vector<std::pair<char, int>>& a) {
+            if (!a.empty()) {
+                a.pop_back();
+            }
         }
-        bool match(char a, char b)
-        {
-            if (a == '(' && b == ')')
-            {
-                return true;
-            }
-            else if (a == '[' && b == ']')
-            {
-                return true;
-            }
-            else if (a == '{' && b == '}')
-            {
-                return true;
-            }
-            return false;
+        bool match(char a, char b) {
+            return (a == '(' && b == ')') || (a == '[' && b == ']') || (a == '{' && b == '}');
         }
-        bool isOpen(char a)
-        {
+        bool isOpen(char a) {
             return a == '(' || a == '[' || a == '{';
         }
-        bool isClose(char a)
-        {
+        bool isClose(char a) {
             return a == ')' || a == ']' || a == '}';
         }
     public:
-        void validPara(std::string pairs)
-        {
+        void validPara(std::string pairs) {
             std::vector<std::pair<char, int>> parent_stack;
 
-            for (int i = 0; i < pairs.length(); i++)
-            {
-                if (pairs[i] == '(' || pairs[i] == '[' || pairs[i] == '{' || pairs[i] == ')' || pairs[i] == ']' || pairs[i] == '}')
-                {
-                    if (parent_stack.size() == 0)
-                    {
+            for (int i = 0; i < pairs.length(); i++) {
+                if (isOpen(pairs[i])) {
+                    push(parent_stack, std::make_pair(pairs[i], i));
+                } else if (isClose(pairs[i])) {
+                    if (!parent_stack.empty() && match(top(parent_stack).first, pairs[i])) {
+                        pop(parent_stack);
+                    } else {
+                        std::cout << "invalid" << std::endl;
+                        return;
+                    }
+                }
+            }
+            std::cout << (parent_stack.empty() ? "valid" : "invalid") << std::endl;
+        }
+
+        int indexError(std::string pairs) {
+            std::vector<std::pair<char, int>> parent_stack;
+
+            for (int i = 0; i < pairs.length(); i++) {
+                if (isOpen(pairs[i])) {
+                    push(parent_stack, std::make_pair(pairs[i], i));
+                } else if (isClose(pairs[i])) {
+                    if (!parent_stack.empty() && match(top(parent_stack).first, pairs[i])) {
+                        pop(parent_stack);
+                    } else {
+                        return i;
+                    }
+                }
+            }
+
+            return parent_stack.empty() ? -1 : parent_stack.back().second;
+        }
+
+        void minPara(std::string pairs) {
+            std::vector<std::pair<char, int>> parent_stack;
+
+            for (int i = 0; i < pairs.length(); i++) {
+                if (isOpen(pairs[i])) {
+                    push(parent_stack, std::make_pair(pairs[i], i));
+                } else if (isClose(pairs[i])) {
+                    if (!parent_stack.empty() && match(top(parent_stack).first, pairs[i])) {
+                        pop(parent_stack);
+                    } else {
                         push(parent_stack, std::make_pair(pairs[i], i));
                     }
-                    else
-                    {
-                        if (match(top(parent_stack).first, pairs[i]))
-                        {
-                            pop(parent_stack);
-                        }
-                        else
-                        {
-                            push(parent_stack, std::make_pair(pairs[i], i));
-                        }
-                    }
                 }
             }
-            if (parent_stack.size() != 0)
-            {
-                std::cout<<"invalid"<<std::endl;
-                return;
-            }
-            std::cout<<"valid"<<std::endl;
+
+            std::cout << "Min: " << parent_stack.size() << std::endl;
         }
 
-        int indexError(std::string pairs)
-        {
-            std::vector<std::pair<char, int>> squig_stack;
-            std::vector<std::pair<char, int>> hard_stack;
-            std::vector<std::pair<char, int>> smooth_stack;
-            std::vector<int> errors;
-            for (int i = 0; i < pairs.length(); i++)
-            {
-                if (isOpen(pairs[i]) || isClose(pairs[i]))
-                {
-                    if (pairs[i] == '{' || pairs[i] == '}')
-                    {
-                        if (match(top(squig_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(squig_stack).second), errors.end());
-                            pop(squig_stack);
-                        }
-                        else
-                        {
-                            push(squig_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '[' || pairs[i] == ']')
-                    {
-                        if (match(top(hard_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(hard_stack).second), errors.end());
-                            pop(hard_stack);
-                        }
-                        else
-                        {
-                            push(hard_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '(' || pairs[i] == ')')
-                    {
-                        if (match(top(smooth_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(smooth_stack).second), errors.end());
-                            pop(smooth_stack);
-                        }
-                        else
-                        {
-                            push(smooth_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                }
-            }
-            if (errors.size() != 0)
-            {
-                if (isOpen(pairs[errors[0]]))
-                {
-                    int pair_count = 0;
-                    int first_close = -1;
-                    for (int i = errors[0]+1; i < pairs.length(); i++)
-                    {
-                        if (isClose(pairs[i]))
-                        {
-                            if (first_close == -1)
-                            {
-                                first_close = i;
-                            }
-                            pair_count -= 1;
-                        }
-                        else if (isOpen(pairs[i]))
-                        {
-                            pair_count += 1;
-                        }
-                        //std::cout<<"DSDSDSD "<<pairs[i]<<std::endl;
-                    }
-                    if (pair_count < 0)
-                    {
-                        return first_close;
-                    }
-                    return pairs.length();
-                }
-                else if (isClose(pairs[errors[0]]))
-                {
-                    return errors[0];
-                }
-            }
-
-            return -1;
-
-        }
-
-        void minPara(std::string pairs)
-        {
-            std::vector<std::pair<char, int>> squig_stack;
-            std::vector<std::pair<char, int>> hard_stack;
-            std::vector<std::pair<char, int>> smooth_stack;
-            std::vector<int> errors;
-            for (int i = 0; i < pairs.length(); i++)
-            {
-                if (isOpen(pairs[i]) || isClose(pairs[i]))
-                {
-                    if (pairs[i] == '{' || pairs[i] == '}')
-                    {
-                        if (match(top(squig_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(squig_stack).second), errors.end());
-                            pop(squig_stack);
-                        }
-                        else
-                        {
-                            push(squig_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '[' || pairs[i] == ']')
-                    {
-                        if (match(top(hard_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(hard_stack).second), errors.end());
-                            pop(hard_stack);
-                        }
-                        else
-                        {
-                            push(hard_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '(' || pairs[i] == ')')
-                    {
-                        if (match(top(smooth_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(smooth_stack).second), errors.end());
-                            pop(smooth_stack);
-                        }
-                        else
-                        {
-                            push(smooth_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                }
-            }
-
-            std::cout<<"Min: "<<(squig_stack.size() + hard_stack.size() + smooth_stack.size())<<std::endl;
-        }
-
-        void scorePara(std::string pairs)
-        {
-            //std::vector<std::pair<char, int>> parent_stack;
+        void scorePara(std::string pairs) {
             int score = 0;
+            std::vector<std::pair<char, int>> parent_stack;
 
-            std::vector<std::pair<char, int>> squig_stack;
-            std::vector<std::pair<char, int>> hard_stack;
-            std::vector<std::pair<char, int>> smooth_stack;
-            std::vector<int> errors;
-            for (int i = 0; i < pairs.length(); i++)
-            {
-                if (isOpen(pairs[i]) || isClose(pairs[i]))
-                {
-                    if (pairs[i] == '{' || pairs[i] == '}')
-                    {
-                        if (match(top(squig_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(squig_stack).second), errors.end());
-                            pop(squig_stack);
-                            score++;
-                        }
-                        else
-                        {
-                            push(squig_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '[' || pairs[i] == ']')
-                    {
-                        if (match(top(hard_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(hard_stack).second), errors.end());
-                            pop(hard_stack);
-                            score++;
-                        }
-                        else
-                        {
-                            push(hard_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
-                    }
-                    else if (pairs[i] == '(' || pairs[i] == ')')
-                    {
-                        if (match(top(smooth_stack).first, pairs[i]))
-                        {
-                            errors.erase(std::remove(errors.begin(), errors.end(), top(smooth_stack).second), errors.end());
-                            pop(smooth_stack);
-                            score++;
-                        }
-                        else
-                        {
-                            push(smooth_stack, std::make_pair(pairs[i], i));
-                            errors.push_back(i);
-                        }
+            for (int i = 0; i < pairs.length(); i++) {
+                if (isOpen(pairs[i])) {
+                    push(parent_stack, std::make_pair(pairs[i], i));
+                } else if (isClose(pairs[i])) {
+                    if (!parent_stack.empty() && match(top(parent_stack).first, pairs[i])) {
+                        pop(parent_stack);
+                        score++;
+                    } else {
+                        push(parent_stack, std::make_pair(pairs[i], i));
                     }
                 }
             }
 
-            //int sub = (smooth_open - smooth_closed) + (brack_open - brack_closed) + (squig_open - squig_closed);
-            std::cout<<"Balance: "<<score<<std::endl;
+            std::cout << "Balance: " << score << std::endl;
         }
-
 };
 
-
-// Add class Queue here
-class Queue
-{
+class Queue {
     private:
         std::vector<std::string> queue;
-        std::string pop()
-        {
-            auto a = queue[0];
-            queue.erase(queue.begin());
-            return a;
-        }
-        std::string reverse(std::string s)
-        {
-            std::string a = "";
-            for (int i = s.size()-1; i >= 0; i--)
-            {
-                a += s[i];
-            }//std::cout<<"DDDD"<<std::endl;
-            return a;
+        std::string reverse(std::string s) {
+            std::reverse(s.begin(), s.end());
+            return s;
         }
     public:
-        void enqueue(std::string s)
-        {
-            int i = 0;
-            int last_working_i = 0;
-            for (i = 0; i < s.size(); i++)
-            {
-                //std::cout<<((i+1) % 8)<<std::endl;
-                if ((i+1) % 8 == 0)
-                {
-                    queue.push_back(reverse(s.substr(i-7, (i+1) - (i-7))));
-                    last_working_i = i;
-                }
-            }
-            if (last_working_i != i)
-            {
-                //std::cout<<"[]"<<s.substr(i)<<"[]"<<std::endl;
-                queue.push_back(reverse(s.substr(last_working_i+1)));
+        void enqueue(std::string s) {
+            int n = s.length();
+            for (int i = 0; i < n; i += 8) {
+                std::string chunk = s.substr(i, 8);
+                queue.push_back(reverse(chunk));
             }
         }
-        void processMsg()
-        {
-            std::string fin;
-            while (queue.size() != 0)
-            {
-                fin += reverse(pop());
+
+        void processMsg() {
+            std::string message;
+            while (!queue.empty()) {
+                std::string chunk = queue.front();
+                queue.erase(queue.begin());
+                message += reverse(chunk);
             }
-            std::cout<<fin<<std::endl;
+            std::cout << message << std::endl;
         }
 };
 
